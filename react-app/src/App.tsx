a04cbc54605fb194461bdfaefd3db51321c0483a
Kevin <kevinn.sarfoo1@gmail.com>
2024-10-24 21:53:22 +0200

    Initial commit

---
+++ react-app/src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  const [alertVisible, alertVisibility] = useState(false);
+
+  return (
+    // <div>
+    //   {alertVisible && (
+    //     <Alert onClose={() => alertVisibility(false)}>
+    //       <span>This is an alert shown after click the button! </span>
+    //     </Alert>
+    //   )}
+    //   <Button color="danger" onClick={() => alertVisibility(true)}>
+    //     Click me!
+    //   </Button>
+    // </div>
+    <ListGroup
+      items={["New York", "Los Angeles", "San Francisco"]}
+      heading={"Miami"}
+      onSelectItem={function (item: string): void {
+        console.log(item, "clicked!");
+      }}
+    />
+  );
+}
+
+export default App;
---
+++ react-app/src/components/ListGroup/ListGroup.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import "./ListGroup.css";
+
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  // Hook
+  const [selectedIntex, setStelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIntex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setStelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
