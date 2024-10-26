20b6ddd08dfe70e05fe3a75f0942a2696c24bf66
Kevin <kevinn.sarfoo1@gmail.com>
2024-10-26 16:37:04 +0200

    CSS-in-JS

--- react-app/src/components/ListGroup/ListGroup.tsx
+++ react-app/src/components/ListGroup/ListGroup.tsx
@@ -38,3 +38,55 @@ function ListGroup({ items, heading, onSelectItem }: Props) {
 }
 
 export default ListGroup;
+
+import { useState } from "react";
+import "./ListGroup.css";
+import styled from "styled-components";
+
+const List = styled.ul`
+  .list-group {
+    list-style: none;
+    padding: 0;
+  }
+`;
+
+interface ListItemProps {
+  active: boolean;
+}
+const ListItem = styled.li<ListItemProps>`
+  padding: 5px 0;
+  background: ${(props) => (props.active ? "blue" : "none")};
+`;
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  // Hook
+  const [selectedIntex, setStelectedIndex] = useState(0);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <List>
+        {items.map((item, index) => (
+          <ListItem
+            active={index === selectedIntex}
+            key={item}
+            onClick={() => {
+              setStelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
+}
+
+export default ListGroup;
