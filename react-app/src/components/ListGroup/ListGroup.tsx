5d5dbb9fc7b071460c7a9fd67ea8de6e3df56235
Kevin <kevinn.sarfoo1@gmail.com>
2024-10-26 16:37:04 +0200

    Repair CSS-in-JSS

--- react-app/src/components/ListGroup/ListGroup.tsx
+++ react-app/src/components/ListGroup/ListGroup.tsx
@@ -1,46 +1,4 @@
 import { useState } from "react";
-import styles from "./ListGroup.module.css";
-
-interface Props {
-  items: string[];
-  heading: string;
-  onSelectItem: (item: string) => void;
-}
-
-function ListGroup({ items, heading, onSelectItem }: Props) {
-  // Hook
-  const [selectedIntex, setStelectedIndex] = useState(-1);
-
-  return (
-    <>
-      <h1>{heading}</h1>
-      {items.length === 0 && <p>No item found</p>}
-      <ul className={[styles.listGroup, styles.containter].join(" ")}>
-        {items.map((item, index) => (
-          <li
-            className={
-              selectedIntex === index
-                ? "list-group-item active"
-                : "list-group-item"
-            }
-            key={item}
-            onClick={() => {
-              setStelectedIndex(index);
-              onSelectItem(item);
-            }}
-          >
-            {item}
-          </li>
-        ))}
-      </ul>
-    </>
-  );
-}
-
-export default ListGroup;
-
-import { useState } from "react";
-import "./ListGroup.css";
 import styled from "styled-components";
 
 const List = styled.ul`
