f469430271c0d97f2b75a56dfb62ad65102a0126
Kevin <kevinn.sarfoo1@gmail.com>
2024-10-28 21:16:45 +0100

    Simplyfying Update Logic with 'Immer' library

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import produce from "immer";
 
 function App() {
   const [bugs, setBugs] = useState([
@@ -7,12 +8,26 @@ function App() {
     { id: 2, title: "Bug 2", fixed: false },
   ]);
   const handleClick = () => {
-    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+    // setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+
+    // simple way to update an obect with 'immer' library
+    // remember that, we have to install this library in the project at first
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed = true;
+      })
+    );
   };
 
   return (
     <div>
-      {bugs.map((bug) => bug.title.toString() + bug.fixed.toString() + " ")}
+      {/* {bugs.map((bug) => bug.title.toString() + bug.fixed.toString() + " ")} */}
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title} {bug.fixed ? "Fixed" : "New"}
+        </p>
+      ))}
 
       <button onClick={handleClick}>Click me!</button>
     </div>
