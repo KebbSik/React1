d5b11ff9af0755614fbbacf798cf79a3cf2a5462
Kevin <kevinn.sarfoo1@gmail.com>
2024-10-28 22:43:50 +0100

    Exercise - Updating States

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,22 +1,41 @@
 import { useState } from "react";
 import "./App.css";
 import produce from "immer";
-import NavBar from "./components/NavBar/NavBar";
-import Cart from "./components/Cart/Cart";
 
 function App() {
-  const [cartItems, setCartItems] = useState([
-    "Product 1",
-    "Product 2",
-    "Producet 3",
-  ]);
-
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
+  });
+  const handleClick = () => {
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
+  };
   return (
     <div>
-      <NavBar cartItemsCount={cartItems.length}></NavBar>
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])}></Cart>
+      {cart.items.map((item) => (
+        <p key={item.title}>
+          {item.title}, Count: {item.quantity}
+        </p>
+      ))}
+      <button onClick={handleClick}>Increse count</button>
     </div>
   );
 }
 
 export default App;
+
+// setBugs(
+//   produce((draft) => {
+//     const bug = draft.find((bug) => bug.id === 1);
+//     if (bug) bug.fixed = true;
+//   })
+// );
+// };
