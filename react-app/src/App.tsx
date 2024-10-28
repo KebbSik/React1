a0a4bac69c40ce31b6fa7bceefdf6f337ec42559
Kevin <kevinn.sarfoo1@gmail.com>
2024-10-28 19:25:31 +0100

    Keeping components Pure

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,26 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import Message from "./components/Message/Message";
 
 function App() {
-  // const [firstName, setfirstName] = useState("Kevin");
-  // const [lastName, setlastName] = useState("Sarfo");
-  const [person, setPerson] = useState({
-    firstName: "",
-    lastName: "",
-  });
-  const [isLoading, setLoading] = useState(false);
-
-  const handleClick = () => {
-    setPerson({
-      firstName: "Kevin",
-      lastName: "Sarfo",
-    });
-  };
-
   return (
     <div>
-      <button onClick={handleClick}>Change Data</button>
-      {person.firstName} {person.lastName}
+      <Message />
+      <Message />
+      <Message />
     </div>
   );
 }
---
+++ react-app/src/components/Message/Message.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// let count = 0;
+
+const Message = () => {
+  let count = 0;
+  count++;
+  return <div>Message {count}</div>;
+};
+
+export default Message;
