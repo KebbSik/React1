908feaaefbf871126b64c6292e201178a5df3110
Kevin <kevinn.sarfoo1@gmail.com>
2024-10-28 19:32:58 +0100

    Understanding the Strict Mode

--- react-app/src/components/Message/Message.tsx
+++ react-app/src/components/Message/Message.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-// let count = 0;
+let count = 0;
 
 const Message = () => {
-  let count = 0;
+  //   let count = 0;
+  console.log("Message called", count);
   count++;
   return <div>Message {count}</div>;
 };
