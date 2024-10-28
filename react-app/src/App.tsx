17623f5527cab73025e00fd65c6d9e8a502f51d1
Kevin <kevinn.sarfoo1@gmail.com>
2024-10-28 18:57:30 +0100

    Understanding the State Hook

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,31 +1,19 @@
 import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button/Button";
-import ListGroup from "./components/ListGroup";
 import "./App.css";
-import { BsCalendar2DateFill } from "react-icons/bs";
-import Like from "./components/Like/Like";
 
 function App() {
-  const [alertVisible, alertVisibility] = useState(false);
+  const [isVisible, setVisiblity] = useState(false);
+  let count = 0;
+  const [isApproved, setApproved] = useState(true);
 
+  const handleClick = () => {
+    setVisiblity(true);
+    count++;
+    console.log(isVisible, count);
+  };
   return (
     <div>
-      <ListGroup
-        items={["New York", "Los Angeles", "San Francisco"]}
-        heading={"Miami"}
-        onSelectItem={function (item: string): void {
-          console.log(item, "clicked!");
-        }}
-      />
-      <BsCalendar2DateFill color="red" size="40" />
-      <Button
-        color="primary"
-        onClick={() => console.log("Button has been clicked!")}
-      >
-        Click me please!
-      </Button>
-      <Like onClick={() => console.log("clicked")} />
+      <button onClick={handleClick}>Show</button>
     </div>
   );
 }
