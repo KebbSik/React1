a8d7263244c4132169a1aafdc45c3bc36730355e
Kevin <kevinn.sarfoo1@gmail.com>
2024-10-28 21:42:56 +0100

    Sharing state between components

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,35 +1,20 @@
 import { useState } from "react";
 import "./App.css";
 import produce from "immer";
+import NavBar from "./components/NavBar/NavBar";
+import Cart from "./components/Cart/Cart";
 
 function App() {
-  const [bugs, setBugs] = useState([
-    { id: 1, title: "Bug 1", fixed: false },
-    { id: 2, title: "Bug 2", fixed: false },
+  const [cartItems, setCartItems] = useState([
+    "Product 1",
+    "Product 2",
+    "Producet 3",
   ]);
-  const handleClick = () => {
-    // setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
-
-    // simple way to update an obect with 'immer' library
-    // remember that, we have to install this library in the project at first
-    setBugs(
-      produce((draft) => {
-        const bug = draft.find((bug) => bug.id === 1);
-        if (bug) bug.fixed = true;
-      })
-    );
-  };
 
   return (
     <div>
-      {/* {bugs.map((bug) => bug.title.toString() + bug.fixed.toString() + " ")} */}
-      {bugs.map((bug) => (
-        <p key={bug.id}>
-          {bug.title} {bug.fixed ? "Fixed" : "New"}
-        </p>
-      ))}
-
-      <button onClick={handleClick}>Click me!</button>
+      <NavBar cartItemsCount={cartItems.length}></NavBar>
+      <Cart cartItems={cartItems} onClear={() => setCartItems([])}></Cart>
     </div>
   );
 }
