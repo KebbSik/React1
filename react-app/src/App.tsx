580b71bb6be4e986bcd18fc53a728aa5e68e40f6
Kevin <kevinn.sarfoo1@gmail.com>
2024-11-05 19:45:02 +0100

    Expense List Mosh - Create a List

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -4,10 +4,50 @@ import produce from "immer";
 import ExpandableText from "./components/ExpandableText/ExpandableText";
 import Form from "./components/Form/Form";
 import ExpenseTracker from "./components/ExpenseTracker/ExpenseTracker";
+import ExpenseList from "./components/expense-tracker/components/ExpenseList";
 function App() {
+  const [expenses, setExpenses] = useState([
+    {
+      id: 1,
+      description: "milk",
+      amount: 5,
+      category: "groceries",
+    },
+    {
+      id: 2,
+      description: "eggs",
+      amount: 10,
+      category: "groceries",
+    },
+    {
+      id: 3,
+      description: "electricity",
+      amount: 100,
+      category: "utilities",
+    },
+    {
+      id: 4,
+      description: "movies",
+      amount: 15,
+      category: "entertainment",
+    },
+    {
+      id: 5,
+      description: "milk",
+      amount: 5,
+      category: "groceries",
+    },
+  ]);
+
   return (
     <div>
-      <ExpenseTracker></ExpenseTracker>
+      {/* <ExpenseTracker></ExpenseTracker> */}
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => {
+          setExpenses(expenses.filter((e) => e.id !== id));
+        }}
+      ></ExpenseList>
     </div>
   );
 }
