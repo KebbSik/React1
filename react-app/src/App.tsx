091bbd991cf5d859aadece3de075409198473d0c
Kevin <kevinn.sarfoo1@gmail.com>
2024-11-05 21:23:09 +0100

    Expense List Mosh - Add requirements to form

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -7,8 +7,7 @@ import ExpenseTracker from "./components/ExpenseTracker/ExpenseTracker";
 import ExpenseList from "./components/expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./components/expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./components/expense-tracker/components/ExpenseForm";
-
-export const categories = ["Groceries", "Utilities", "Entertainment"];
+import categories from "./components/expense-tracker/categories";
 
 function App() {
   const [expenses, setExpenses] = useState([
