f7e1fd5452942f21b0c88e0551dc31bb0154e14c
Kevin <kevinn.sarfoo1@gmail.com>
2024-11-09 17:23:16 +0100

    Showing a Loading Indicator

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -12,19 +12,25 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
-
+    setLoading(true);
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users", {
         signal: controller.signal,
       })
-      .then((response) => setUsers(response.data))
+      .then((response) => {
+        setUsers(response.data);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
+    // .finally(() => setLoading(false)); //better aproach but does't work at strict mode
 
     return () => {
       controller.abort();
@@ -34,6 +40,8 @@ function App() {
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
+      {/* To see the loader for longer we shold change throttling  to low connection (for example 3G), in browsers webDevTool > Network  */}
+      {isLoading && <div className="spinner-border"></div>}
       <ul className="list-group">
         {users.map((user) => (
           <li className="list-group-item center" key={user.id}>
